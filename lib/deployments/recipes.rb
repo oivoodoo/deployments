b4115a09c376255b4a678212dc0239d9993002f5
Alexandr Korsak <alex.korsak@gmail.com>
2012-06-15 14:48:36 +0300

    Add logger messages

--- lib/deployments/recipes.rb
+++ lib/deployments/recipes.rb
@@ -1,10 +1,24 @@
 # Capistrano Recipes for managing delayed_job
 
 Capistrano::Configuration.instance.load do
-  after 'deploy' do
+  after 'deploy:migrations' do
+    logger.info "Run rake deployments:push app_env=#{rails_env} on deploy:migrations"
+
     sh <<-CMD
       rake deployments:push app_env=#{rails_env}
     CMD
+
+    logger.info "Deployments push completed."
+  end
+
+  after 'deploy' do
+    logger.info "Run rake deployments:push app_env=#{rails_env} on deploy"
+
+    sh <<-CMD
+      rake deployments:push app_env=#{rails_env}
+    CMD
+
+    logger.info "Deployments push completed."
   end
 end
 
