1cca045991818752caeb3d8ea0f437981b6988a4
Alexandr Korsak <alex.korsak@gmail.com>
2012-06-15 15:38:57 +0300

    Add to cap recipe making link version.txt

--- lib/deployments/recipes.rb
+++ lib/deployments/recipes.rb
@@ -1,16 +1,19 @@
 # Capistrano Recipes for managing delayed_job
 
 Capistrano::Configuration.instance.load do
-  after 'deploy:migrations' do
-    run <<-CMD
-      cd #{current_path} && rake deployments:push app_env=#{rails_env}
-    CMD
-  end
+  after 'deploy:migrations', 'deployments:push'
+  after 'deploy',            'deployments:push'
 
-  after 'deploy' do
-    run <<-CMD
-      cd #{current_path} && rake deployments:push app_env=#{rails_env}
-    CMD
+  namespace :deployments do
+    task :push do
+      run <<-CMD
+        cd #{current_path}/public && ln -s #{shared_path}/public/version.txt version.txt
+      CMD
+
+      run <<-CMD
+        cd #{current_path} && rake deployments:push app_env=#{rails_env}
+      CMD
+    end
   end
 end
 
