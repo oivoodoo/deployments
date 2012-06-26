efb2fab6033a9af623653bf4f622965915b661ba
Alexandr Korsak <alex.korsak@gmail.com>
2012-06-26 13:50:17 +0300

    Send report about deployment only once for primary server using roles as db

--- lib/deployments/recipes.rb
+++ lib/deployments/recipes.rb
@@ -5,12 +5,9 @@ Capistrano::Configuration.instance.load do
   after 'deploy',            'deployments:push'
 
   namespace :deployments do
-    task :push do
+    task :push, :roles => :db do
       run <<-CMD
         cd #{current_path}/public && ln -s #{shared_path}/public/version.txt version.txt
-      CMD
-
-      run <<-CMD
         cd #{current_path} && rake deployments:push app_env=#{rails_env}
       CMD
     end
