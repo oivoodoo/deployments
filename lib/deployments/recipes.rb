5ed1a08e6f0620bf6cdea1c44a3bc14ed2bc8886
Alexandr Korsak <alex.korsak@gmail.com>
2012-06-27 09:07:39 +0300

    Split commands of recipe

--- lib/deployments/recipes.rb
+++ lib/deployments/recipes.rb
@@ -8,6 +8,9 @@ Capistrano::Configuration.instance.load do
     task :push, :roles => :db do
       run <<-CMD
         cd #{current_path}/public && ln -s #{shared_path}/public/version.txt version.txt
+      CMD
+
+      run <<-CMD
         cd #{current_path} && rake deployments:push app_env=#{rails_env}
       CMD
     end
